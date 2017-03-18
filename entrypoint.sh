4e421b894295beea73ac8a547a383cfca7673e54
Jon Schulberger <jschoulzy@gmail.com>
2017-03-18 19:44:26 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cd /tmp
-sudo rm -f *
+sudo rm -f /tmp/*
 
 mkdir -p /root/.vnc
 
-echo $VNCPASS | vncpasswd -f > /root/.vnc/passwd
+echo $VNCPASS | vncpasswd -f > /home/atlauncher/.vnc/passwd
 
 vncserver :1 -geometry 1280x800 -depth 24
