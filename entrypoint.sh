268781d60363a65b92a29da2aac992edc5e952c2
Jon Schulberger <jschoulzy@gmail.com>
2017-03-18 18:50:15 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 cd /tmp
 sudo rm -f *
 
-mkdir /root/vncpass
 echo $VNCPASS | vncpasswd -f > /root/vncpass
 
-vncserver root:1 -geometry 1280x800 -depth 24
+vncserver :1 -geometry 1280x800 -depth 24
